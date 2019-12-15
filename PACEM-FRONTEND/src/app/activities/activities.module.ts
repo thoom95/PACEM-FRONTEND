5aff0bafe944f9e5d0e906722f03d9d3681a83cd
MaandagDev <D.Alper1@student.han.nl>
2019-12-15 22:48:48 +0100

    Basic design for activities is done

--- PACEM-FRONTEND/src/app/activities/activities.module.ts
+++ PACEM-FRONTEND/src/app/activities/activities.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {ActivitiesPage} from './activities.page';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: ActivitiesPage }])
-  ],
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([{path: '', component: ActivitiesPage}]),
+        FontAwesomeModule
+    ],
   declarations: [ActivitiesPage]
 })
 export class InvitesPageModule {}
