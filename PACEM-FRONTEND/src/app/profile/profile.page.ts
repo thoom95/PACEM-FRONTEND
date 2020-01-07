fd068b12a6997d75e48a70034bbfaaadcddee646
MaandagDev <D.Alper1@student.han.nl>
2020-01-07 09:29:48 +0100

    Minor fixes to profile page

--- PACEM-FRONTEND/src/app/profile/profile.page.ts
+++ PACEM-FRONTEND/src/app/profile/profile.page.ts
@@ -1,6 +1,8 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import {ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import {Hobby, ProfileDomain} from '../models/domain-model/profile.domain';
 import {ProfileService} from './service/profile.service';
+import {DomSanitizer} from '@angular/platform-browser';
+import {IonInput} from '@ionic/angular';
 
 @Component({
     selector: 'app-profile',
@@ -9,11 +11,40 @@ import {ProfileService} from './service/profile.service';
 })
 export class ProfilePage {
     private profileDomain: ProfileDomain;
+    public editStatus: boolean;
 
-    constructor(private profileService: ProfileService, private changeDetectorRef: ChangeDetectorRef) {
+    constructor(private profileService: ProfileService, private changeDetectorRef: ChangeDetectorRef, private domSen: DomSanitizer) {
         this.profileService.getProfileInfo().then((profileDomain) => {
             this.profileDomain = profileDomain;
+            console.log(profileDomain);
             this.changeDetectorRef.detectChanges();
         });
+    }
+
+    @ViewChild('statusField', {static: true}) statusField: IonInput;
+    editAboutMe: boolean;
+
+    public saveStatus($event: any) {
+        const status = $event.target.value;
+        if (status && status.length > 1) {
+            this.profileService.setProfileStatus($event.target.value);
+            this.profileService.getProfileInfo().then((profileDomain) => {
+                this.profileDomain = profileDomain;
+                this.changeDetectorRef.detectChanges();
+                this.editStatus = false;
+                this.statusField.value = '';
+            });
+        }
+    }
+
+    enableStatusField() {
+        this.editStatus = true;
+
+        this.changeDetectorRef.detectChanges();
+        this.statusField.setFocus();
+    }
+
+    saveAboutMe($event: any) {
+
     }
 }
