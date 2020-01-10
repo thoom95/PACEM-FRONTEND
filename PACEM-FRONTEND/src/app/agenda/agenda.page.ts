970dbac151dcf959929d3eca6e926f52c394adeb
Bart Kessels <development@bk-mail.com>
2020-01-10 10:02:41 +0100

    Add agendascreen

--- PACEM-FRONTEND/src/app/agenda/agenda.page.ts
+++ PACEM-FRONTEND/src/app/agenda/agenda.page.ts
@@ -2,117 +2,45 @@ import { CalendarComponent } from 'ionic2-calendar/calendar';
 import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { formatDate } from '@angular/common';
+import {ActivitiesService} from '../activities/service/activities.service';
 
 @Component({
   selector: 'app-agenda',
   templateUrl: 'agenda.page.html',
   styleUrls: ['agenda.page.scss'],
 })
-export class AgendaPage implements OnInit {
+export class AgendaPage {
 
-  event = {
-    title: '',
-    desc: '',
-    startTime: '',
-    endTime: '',
-    allDay: false
-  };
-  minDate = new Date().toISOString();
-  eventSource = [];
-  viewTitle;
-  calendar = {
-    mode: 'month',
-    currentDate: new Date(),
-  };
+  public events = [];
+  public currentDate = new Date();
 
-  // AANPASSING
   @ViewChild(CalendarComponent, null) myCal: CalendarComponent;
 
-  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
-
-  ngOnInit() {
-    this.resetEvent();
+  constructor(private alertCtrl: AlertController, private activitiesService: ActivitiesService, @Inject(LOCALE_ID) private locale: string) {
+    this.activitiesService.getEvents().subscribe((activities) => {
+      this.events = activities.map((activity) => {
+        return {
+          title: activity.name,
+          desc: activity.location.info,
+          startTime: new Date(Number(activity.startTime) * 1000),
+          endTime: new Date(Number(activity.endTime) * 1000),
+          allDay: false
+        };
+      });
+    });
   }
 
-  resetEvent() {
-    this.event = {
-      title: '',
-      desc: '',
-      startTime: new Date().toISOString(),
-      endTime: new Date().toISOString(),
-      allDay: false
-    };
-  }
-
-  // Create the right event format and reload source
-  addEvent() {
-    const eventCopy = {
-      title: this.event.title,
-      startTime: new Date(this.event.startTime),
-      endTime: new Date(this.event.endTime),
-      allDay: this.event.allDay,
-      desc: this.event.desc
-    };
-
-    if (eventCopy.allDay) {
-      const start = eventCopy.startTime;
-      const end = eventCopy.endTime;
-
-      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
-      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
-    }
-
-    this.eventSource.push(eventCopy);
-    this.myCal.loadEvents();
-    this.resetEvent();
-  }
-
-  // Change current month/week/day
-  next() {
-    const swiper = document.querySelector('.swiper-container')['swiper'];
-    swiper.slideNext();
-  }
-
-  back() {
-    const swiper = document.querySelector('.swiper-container')['swiper'];
-    swiper.slidePrev();
-  }
-
-  // Change between month/week/day
-  changeMode(mode) {
-    this.calendar.mode = mode;
-  }
-
-  // Focus today
-  today() {
-    this.calendar.currentDate = new Date();
-  }
-
-  // Selected date reange and hence title changed
-  onViewTitleChanged(title) {
-    this.viewTitle = title;
-  }
-
-  // Calendar event was clicked
   async onEventSelected(event) {
-    // Use Angular date pipe for conversion
     const start = formatDate(event.startTime, 'medium', this.locale);
     const end = formatDate(event.endTime, 'medium', this.locale);
 
     const alert = await this.alertCtrl.create({
       header: event.title,
       subHeader: event.desc,
-      message: 'From: ' + start + '<br><br>To: ' + end,
+      message: 'Van: ' + start + '<br><br>Tot: ' + end,
       buttons: ['OK']
     });
-    alert.present();
-  }
 
-  // Time slot was clicked
-  onTimeSelected(ev) {
-    const selected = new Date(ev.selectedTime);
-    this.event.startTime = selected.toISOString();
-    selected.setHours(selected.getHours() + 1);
-    this.event.endTime = (selected.toISOString());
+    await alert.present();
   }
 }
